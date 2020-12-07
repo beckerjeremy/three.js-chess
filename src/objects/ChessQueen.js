37f94a38e49c3cf5117a12f88b8569d11564e102
Jeremy Becker <beckerjeremy@outlook.com>
2020-12-07 15:40:51 +0100

    Added queen piece and move pattern

---
+++ src/objects/ChessQueen.js
@@ -0,0 +1,55 @@
+import ChessPiece from './ChessPiece';
+import ChessUtils from './ChessUtils';
+
+const THREE = require("three-js")();
+
+class ChessQueen extends ChessPiece {
+
+    constructor( color, fenPosition = null ) {
+        super( color, fenPosition, new THREE.BoxBufferGeometry( .3, .8, .3 ) );
+
+    }
+
+    getPossibleMoves( board ) {
+        let moves = [];
+        let positionVector = ChessUtils.positionToVector2( this.fenPosition );
+
+        // Check rook moves
+        let directions = [ -1, 1, 0, 0 ];
+        for(let i = 0; i < 4; i ++) {
+            let xDirection = directions[i];
+            let yDirection = directions[directions.length - 1 - i];
+            for( let j = 1; j < 8; j++ ) {
+                let tempPosition = board.getFiledByPositionCoordinates( positionVector.x + j * xDirection, positionVector.y + j * yDirection );
+                if( tempPosition != null ) {
+                    if( tempPosition.piece != null ) {
+                        if( tempPosition.piece.color != this.color ) moves.push( tempPosition );
+                        break;
+                    }
+                    else moves.push( tempPosition );
+                } 
+                else break;
+            }
+        }
+
+        // Check bishop moves
+        let xDirections = [ -1, 1, -1, 1 ];
+        let yDirections = [ -1, -1, 1, 1 ];
+        for(let i = 0; i < 4; i ++) {
+            for( let j = 1; j < 8; j++ ) {
+                let tempPosition = board.getFiledByPositionCoordinates( positionVector.x + j * xDirections[i], positionVector.y + j * yDirections[i] );
+                if( tempPosition != null ) {
+                    if( tempPosition.piece != null ) {
+                        if( tempPosition.piece.color != this.color ) moves.push( tempPosition );
+                        break;
+                    }
+                    else moves.push( tempPosition );
+                } 
+                else break;
+            }
+        }
+    }
+
+}
+
+export default ChessQueen;
