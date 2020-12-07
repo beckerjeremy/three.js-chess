dd25157dc31f934c702843ae8889cfca9f5c8e33
Jeremy Becker <beckerjeremy@outlook.com>
2020-12-07 15:36:56 +0100

    Added bishop piece and move pattern

---
+++ src/objects/ChessBishop.js
@@ -0,0 +1,36 @@
+import ChessPiece from './ChessPiece';
+import ChessUtils from './ChessUtils';
+
+const THREE = require("three-js")();
+
+class ChessBishop extends ChessPiece {
+
+    constructor( color, fenPosition = null ) {
+        super( color, fenPosition, new THREE.BoxBufferGeometry( .3, .8, .3 ) );
+
+    }
+
+    getPossibleMoves( board ) {
+        let moves = [];
+        let positionVector = ChessUtils.positionToVector2( this.fenPosition );
+
+        let xDirections = [ -1, 1, -1, 1 ];
+        let yDirections = [ -1, -1, 1, 1 ];
+        for(let i = 0; i < 4; i ++) {
+            for( let j = 1; j < 8; j++ ) {
+                let tempPosition = board.getFiledByPositionCoordinates( positionVector.x + j * xDirections[i], positionVector.y + j * yDirections[i] );
+                if( tempPosition != null ) {
+                    if( tempPosition.piece != null ) {
+                        if( tempPosition.piece.color != this.color ) moves.push( tempPosition );
+                        break;
+                    }
+                    else moves.push( tempPosition );
+                } 
+                else break;
+            }
+        }
+    }
+
+}
+
+export default ChessBishop;
